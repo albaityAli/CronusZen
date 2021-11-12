181b8141014022265d37fd99cac2a20fb5b08d5c
Ali Al-baity <84631973+kingAli110@users.noreply.github.com>
2021-11-12 20:37:03 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from discord.ext import commands, tasks
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 import checkingOutProperly
+import discord
+
+#subsitute john with the name of the instance you created in checkingOutProperly
+
+def read(driver, locator):
+    driver.implicitly_wait(10)
+    element = driver.find_element_by_xpath(locator).text
+    return element
 
 
 driver = webdriver.Chrome(executable_path=r"C:\Users\aalba\OneDrive\Desktop\chromedriver.exe")
@@ -25,17 +33,17 @@ async def on_ready():
 async def check_cronus_zen():
 
     x = checkCronusZen()
-    if x == "SOLD OUT":
+    if x != "SOLD OUT":
         global count
         count += 1
-        channel = await client.fetch_channel(#channel_id)
+        channel = await client.fetch_channel("insert the channel you want the messages to be sent to here")
 
-        #await channel.send("CRONUS ZEN IS IN STOCK @everyone! I am buying them right now! https://collectiveminds-uk.myshopify.com/products/cronus-zen")
-        await channel.send(#call buy function from checkingOutProperly after creating instances))
+        await channel.send("CRONUS ZEN IS IN STOCK @everyone! I am buying them right now! https://collectiveminds-uk.myshopify.com/products/cronus-zen")
+        await channel.send(checkingOutProperly.john.buy(1))
 
 
     elif x == "TimeoutException":
-        channel = await client.fetch_channel(836377623252303883)
+        channel = await client.fetch_channel("insert the channel you want the messages to be sent to here")
         await channel.send("Sorry, there has been an error. Please send the command again")
 
 
@@ -53,12 +61,12 @@ async def successful_order(ctx):
         title='ORDER SUCCESSFUL',
         colour=discord.Colour.green()
     )
-    channel = await client.fetch_channel(#channel_id)
+    channel = await client.fetch_channel("insert the channel you want the messages to be sent to here")
     nicely.set_thumbnail(url='https://cdn.shopify.com/s/files/1/0306/9558/7884/products/cronus_zen_trans_shadow_800_1024x1024.png?v=1588706228')
     nicely.add_field(name="Product Name", value="Cronus Zen", inline=False)
-    nicely.add_field(name="Name", value=f"{checkingOutProperly.Ali.name_on_card}   ", inline=True)
-    nicely.add_field(name="Email", value=checkingOutProperly.Ali.email, inline=True)
-    nicely.set_footer(text=date)
+    nicely.add_field(name="Name", value=f"{checkingOutProperly.john.name_on_card}   ", inline=True)
+    nicely.add_field(name="Email", value=checkingOutProperly.john.email, inline=True)
+    #nicely.set_footer(text=date)
 
 
     await channel.send(embed=nicely)
@@ -71,22 +79,15 @@ async def failed_order(ctx):
         title='ORDER SUCCESSFUL',
         colour=discord.Colour.green()
     )
-    channel = await client.fetch_channel(#channel_id)
+    channel = await client.fetch_channel("insert the channel you want the messages to be sent to here")
     nicely.set_thumbnail(url='https://cdn.shopify.com/s/files/1/0306/9558/7884/products/cronus_zen_trans_shadow_800_1024x1024.png?v=1588706228')
     nicely.add_field(name="Product Name", value="Cronus Zen", inline=False)
-    nicely.add_field(name="Name", value=f"{checkingOutProperly.Ali.name_on_card}   ", inline=True)
-    nicely.add_field(name="Email", value=checkingOutProperly.Ali.email, inline=True)
-    nicely.set_footer(text=date)
+    nicely.add_field(name="Name", value=f"{checkingOutProperly.john.name_on_card}   ", inline=True)
+    nicely.add_field(name="Email", value=checkingOutProperly.john.email, inline=True)
+    #nicely.set_footer(text=date)
 
 
     await channel.send(embed=nicely)
 
 
-
-def read(driver, locator):
-    driver.implicitly_wait(10)
-    element = driver.find_element_by_xpath(locator).text
-    return element
-
-
-client.run(#discord api key)
+client.run("insert your discord application bot token here")
